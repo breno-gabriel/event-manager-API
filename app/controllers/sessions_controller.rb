098a651e7d7dd03eade1e4b570a612c5c8aa2285
Breno <brenomelogabriel2019@gmail.com>
2026-02-28 13:09:16 -0300

    feat: filtra o json do usuario para não revelar a senha.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: user.id)
-      render json: { token: token }, status: :ok
+      render json: { token: token, user: user }, status: :ok
     else
       render json: { error: "Invalid credentials" }, status: :unauthorized
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     validates :email, presence: true, uniqueness: true
+
+    def as_json(options = {})
+        super(options.merge(except: [:password_digest]))
+    end
+
 end
