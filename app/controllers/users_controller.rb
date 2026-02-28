3f344e8909092054d7017a26362e69e01d7bae0b
Breno <brenomelogabriel2019@gmail.com>
2026-02-28 13:01:22 -0300

    feat: adiciona registro e autenticação de usuário

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_user, only: [:create]
+   
+  def create
+    user = User.new(user_params)
+    if user.save
+      token = JsonWebToken.encode(user_id: user.id)
+      render json: { token: token, user: user }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @current_user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
